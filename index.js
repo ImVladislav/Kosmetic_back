e48096550fb27f22f2191471851bf8cc8c76e5fe
Vladislav <nafanya102@gmail.com>
2025-05-07 15:40:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,6 @@ const swaggerDocs = require("./docs/swagger"); // Підключення до sw
 const emailRouter = require("./email");
 
 
-const db = require("./models"); // models/index.js
-const router = require("./routes/index");
-const errorHandler = require("./middlewares/ErrorHandlingMiddleware");
-const swaggerDocs = require("./docs/swagger"); // ТВОЙ swagger – залишаємо обов'язково
-const emailRouter = require("./email");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
