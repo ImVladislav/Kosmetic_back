6a531a13c383af3a3991289e3108cf8717581b78
Vladislav <nafanya102@gmail.com>
2025-05-07 16:14:43 +0300

    Update emailController.js

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 const mailer = require("../helpers/mailer"); // шлях до нового mailer
-const { ctrlWrapper } = require("../helpers");
+const { ctrlWrapper } = require("../helpers/ApiError");
+
 
 const sendEmail = async (paths, req, res) => {
   const { title, text, to, subject } = req.body;
