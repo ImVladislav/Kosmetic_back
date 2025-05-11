83236540fffad9e47c97bf0b5d23b6d5f0bbe37a
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2025-05-11 19:29:51 +0300

    fix: change cors

--- routes/filterTags.js
+++ routes/filterTags.js
@@ -12,5 +12,49 @@ router.post(
   upload.single("file"),
   filterTagController.importFilterTagFromExcel
 );
+/**
+ * @swagger
+ * /filter-tags/import:
+ *   post:
+ *     summary: Отримати всі фільтри
+ *     tags: [filter-tags]
+ *     description: Отримати всі фільтри
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - file
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *     responses:
+ *       200:
+ *         description: Фільтри успішно завантажені
+ */
+
+// Отримати всі фільтри
+router.get("/", filterTagController.getAllFilterTags);
+/**
+ * @swagger
+ * /filter-tags:
+ *   get:
+ *     summary: Отримати всі фільтри
+ *     tags: [filter-tags]
+ *     description: Отримати всі фільтри
+ *     responses:
+ *       200:
+ *         description: Фільтри успішно отримані
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 $ref: "#/components/schemas/FilterTag"
+ */
 
 module.exports = router;
